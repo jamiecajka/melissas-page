d3a141c391f5fbe1c7723dc54940574645f20076
Jamie Cajka <jamiecajka@gmail.com>
2020-08-11 12:51:11 -0500

    added carrierwave/picture to posts and made post index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_032928) do
+ActiveRecord::Schema.define(version: 2020_08_11_171751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_032928) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body", null: false
+    t.string "picture", default: "db/images/default.jpg"
   end
 
   create_table "services", force: :cascade do |t|
